249a1154a94d295b3c66fd7cbfc24036e52c0b5f
chinedumc <chibokachinedum@gmail.com>
2020-10-05 23:32:32 +0100

    THIS in Methods

---
+++ New Modern JS Bootcamp 2020/20thisInMethods.js
@@ -0,0 +1,14 @@
+const person = {
+    first: 'Cherilyn',
+    last: 'Sarkisian',
+    nickName: 'Cher',
+    fullName() {
+        console.log(`${this.first} ${this.last}, AKA ${this.nickName}`)
+    }
+}
+
+// can also be written as:
+    fullName() {
+        const {first, last, nickName} = this;
+        console.log(`${first} ${last} AKA ${nickName}`)
+    }
