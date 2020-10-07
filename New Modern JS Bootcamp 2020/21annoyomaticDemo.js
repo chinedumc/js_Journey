979419de7e22c7a412dc51c570fbf3f8e792ffcd
chinedumc <chibokachinedum@gmail.com>
2020-10-07 21:15:20 +0100

    Annoyomatic Demo

---
+++ New Modern JS Bootcamp 2020/21annoyomaticDemo.js
@@ -0,0 +1,13 @@
+const annoyer = {
+    phrases: ["literally", "cray cray", "I can't even", "Totes!", "YOLO", "Can't Stop", "Won't Stop"],
+    pickPhrase() {
+        const {phrases} = this; // Destructuring for phrases = this.phrases
+    const idx = Math.floor(Math.random() * phrases.length);
+    return phrases[idx]
+    },
+    start() {
+        setInterval(() => {
+            console.log(this.pickPhrase())
+        },3000)
+    }
+}
