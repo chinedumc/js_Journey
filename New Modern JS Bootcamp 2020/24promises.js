d2a699dfd354ae7acfd0c8ab4d7afafe091d32c2
chinedumc <chibokachinedum@gmail.com>
2020-10-24 23:13:47 +0100

    Promises

--- New Modern JS Bootcamp 2020/24promises.js
+++ New Modern JS Bootcamp 2020/24promises.js
@@ -1,19 +1,43 @@
-const willGoHome = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        const rand = Math.random();
-        console.log(rand);
-        if (rand < 0.5) {
-            resolve();
-        } else {
-            reject();
-        }
-    }, 2000)
+// const willGoHome = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         const rand = Math.random();
+//         console.log(rand);
+//         if (rand < 0.5) {
+//             resolve();
+//         } else {
+//             reject();
+//         }
+//     }, 2000)
     
-});
+// });
 
-willGoHome.then(() => {
-    console.log( 'Yes!');
-});
-willGoHome.catch(() => {
-    console.log('No home now');
-});
+// willGoHome.then(() => {
+//     console.log( 'Yes!');
+// });
+// willGoHome.catch(() => {
+//     console.log('No home now');
+// });
+
+//RESOLVING - REJECTING WITH VALUES
+const fakeRequest = (url) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const rand = Math.random();
+            if (rand < 0.5) {
+                reject({status: 404});
+            }
+            else {
+                resolve();
+            }
+        }, 1000)
+    });
+};
+
+fakeRequest()
+    .then(() => {
+        console.log('REQUEST WORKED!');
+    })
+    .catch((res) => {
+        console.log(res.status);
+        console.log('REQUEST FAILED');
+    });
