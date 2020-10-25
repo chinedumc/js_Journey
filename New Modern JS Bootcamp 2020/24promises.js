4781676eb10ffc4e70eeeab735b9a3ac2221e05a
chinedumc <chibokachinedum@gmail.com>
2020-10-25 01:24:58 +0100

    Promises: Promise Chaining

--- New Modern JS Bootcamp 2020/24promises.js
+++ New Modern JS Bootcamp 2020/24promises.js
@@ -42,34 +42,89 @@
 //         console.log('REQUEST FAILED');
 //     });
 
-const fakeRequest = (url) => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            const pages = {
-                '/users': [
-                    {id: 1, username: 'Bilbo'},
-                    {id: 2, username: 'Esmerelda'}
-                ],
-                '/about': 'This is the about page!'
-            };
-            const data = pages[url];
-            if (data) {
-                resolve({ status: 200, data});
-            }
-            else {
-                reject({ status: 404})
-            }
-        }, 1000)
-    });
-};
+// const fakeRequest = (url) => {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             const pages = {
+//                 '/users': [
+//                     {id: 1, username: 'Bilbo'},
+//                     {id: 2, username: 'Esmerelda'}
+//                 ],
+//                 '/about': 'This is the about page!'
+//             };
+//             const data = pages[url];
+//             if (data) {
+//                 resolve({ status: 200, data});
+//             }
+//             else {
+//                 reject({ status: 404})
+//             }
+//         }, 1000)
+//     });
+// };
+
+// fakeRequest('/users')
+//     .then((res) => {
+//         console.log('Status Code', res.status);
+//         console.log('Data', res.data);
+//         console.log('REQUEST WORKED!');
+//     })
+//     .catch((res) => {
+//         console.log(res.status);
+//         console.log('REQUEST FAILED');
+//     });
+
+    //PROMISE CHAINING
+    const fakeRequest = (url) => {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                const pages = {
+                    '/users': [
+                        {id: 1, username: 'Bilbo'},
+                        {id: 5, username: 'Esmerelda'}
+                    ],
+                    '/users/1' : {
+                        id: 1,
+                        username: 'Bilbo',
+                        upvotes: 360,
+                        city: 'Lisbon',
+                        topPostId: 454321
+                    },
+                    '/users/5' : {
+                        id: 5,
+                        username: 'Esmerelda',
+                        upvotes: 571,
+                        city: 'Honolulu'
+                    },
+                    'posts/454321': {
+                        id: 454321,
+                        title: 'Introduction of Mr Man'
+                    },
+                    '/about': 'This is the about page!'
+                };
+                const data = pages[url];
+                if (data) {
+                    resolve({ status: 200, data}); //resolve with a value!
+                }
+                else {
+                    reject({ status: 404}); //reject with a value!
+                }
+            }, 1000);
+        });
+    };
 
 fakeRequest('/users')
     .then((res) => {
-        console.log('Status Code', res.status);
-        console.log('Data', res.data);
-        console.log('REQUEST WORKED!');
+        const id = res.data[0].id;
+        return fakeRequest(`/users/${id}`);
     })
-    .catch((res) => {
-        console.log(res.status);
-        console.log('REQUEST FAILED');
-    });
+    .then((res) => {
+        const postId = res.data.topPostId;
+        return fakeRequest(`/posts/${postId}`)
+    })
+    .then((res) => {
+        console.log(res);
+    })
+    .catch((err) => {
+        console.log('OH NO!', err);
+    });    
