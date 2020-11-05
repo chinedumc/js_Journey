fe2d7f51e70eec36db1ad3661d4fd4d02151749f
chinedumc <chibokachinedum@gmail.com>
2020-11-05 20:34:44 +0100

    Project: Timer, Adding a pause feature

--- New Modern JS Bootcamp 2020/timer.js
+++ New Modern JS Bootcamp 2020/timer.js
@@ -5,9 +5,19 @@ class Timer {
         this.pauseButton = pauseButton;
 
         this.startButton.addEventListener('click', this.start);
+        this.pauseButton.addEventListener('click', this.pause);
     }
-    start() {
-        console.log('Start the timer!');
+    start = () => {
+        this.tick();
+        this.interval = setInterval(this.tick, 1000);
+    };
+    //Adding a pause function to stop the timer on click of the pause button
+    pause = () => {
+        clearInterval(this.interval);
+    };
+    //Adding a function which monitors the ticks of the timer
+    tick = () => {
+        console.log('tick');
     }
 
 }
