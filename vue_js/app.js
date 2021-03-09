f6c8fe3296f8a82fdd0aeb4cbac1f510885f0e2a
chinedumc <chibokachinedum@gmail.com>
2021-03-09 01:09:50 +0100

    Displaying One item on webpage

---
+++ vue_js/app.js
@@ -0,0 +1,11 @@
+var data = {
+    'name': "Bamboo Thermal Ski Coat",
+    'description': "You'll be the most environmentally conscious skier on the slopes- and the most stylish - wearing our fitted coat.",
+    'image': "https://hplussport.com/wp-content/uploads/2016/12/ski-coat_LYNDA_29940.jpg",
+    'price': 99
+}
+
+var app = new Vue({
+    el:'#app',
+    data: data
+});
