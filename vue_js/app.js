7faf23a8834e0ba08157206cf9e2b8203c099b0d
chinedumc <chibokachinedum@gmail.com>
2021-03-09 01:45:52 +0100

    Looping through several items to display

--- vue_js/app.js
+++ vue_js/app.js
@@ -1,8 +1,27 @@
 var data = {
-    'name': "Bamboo Thermal Ski Coat",
-    'description': "You'll be the most environmentally conscious skier on the slopes- and the most stylish - wearing our fitted coat.",
-    'image': "https://hplussport.com/wp-content/uploads/2016/12/ski-coat_LYNDA_29940.jpg",
-    'price': 99
+    products: [
+        {
+            'name': "Bamboo Thermal Ski Coat",
+            'description': "You'll be the most environmentally conscious skier on the slopes- and the most stylish - wearing our fitted coat.",
+            'image': "https://hplussport.com/wp-content/uploads/2016/12/ski-coat_LYNDA_29940.jpg",
+            'price': 99
+        },
+
+        {
+            'name': "Slicker Jacket",
+            'description': "Wind and rain are no match for our organic bamboo slicker jacket for men and women. Triple stitched seams, zippered pockets, and a stay-tight hood are just a few features of our best-selling jacket.",
+            'image': "https://hplussport.com/wp-content/uploads/2016/12/slicker-jacket_LYNDA_29941.jpg",
+            'price': 125
+        },
+
+        {
+            'name': "Thermal Fleece Jacket",
+            'description': "Our thermal organic fleece jacket, is brushed on both sides for ultra softness and warmth. This medium-weight jacket is versatile all year around, and can be worn with layers for the winter season.",
+            'image': "https://hplussport.com/wp-content/uploads/2016/12/thermal-jacket_LYNDA_29943.jpg",
+            'price': 60
+        }
+    ]
+    
 }
 
 var app = new Vue({
