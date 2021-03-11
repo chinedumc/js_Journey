b8207613d57a52775f0b030a08434a1ec38ea083
chinedumc <chibokachinedum@gmail.com>
2021-03-11 18:48:36 +0100

    Events and Methods: Add a count for cart items

--- vue_js/app.js
+++ vue_js/app.js
@@ -2,13 +2,14 @@ var app = new Vue({
     el:'#app',
     data: {
         maximum: 99,
-        products: null
+        products: null,
+        cart: []
     },
     mounted: function () {
         fetch('https://hplussport.com/api/products/order/price')
         .then(response => response.json())
         .then(data => {
-            this.products = data;
+            this.products = data; //The data rcvd from the json Object, put it in products and set it to the data recvd
         })        
     }
 });
